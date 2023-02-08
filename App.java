80a903336b775724532733af3487de41da0c0541
Anmol goyal <124735413+hacken08@users.noreply.github.com>
2023-02-08 20:10:17 +0530

    don't know

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        
+        int c = 78;
+        System.out.println("name your my buddy");
+        
+
+    }
+}
